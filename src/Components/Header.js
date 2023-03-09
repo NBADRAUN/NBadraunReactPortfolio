4cf38ef01b65e8b028028942c41fa538fc955d53
NBADRAUN <Nbadraun@gmail.com>
2023-03-09 09:20:08 -0700

    adding inital header component

---
+++ src/Components/Header.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+class Header extends Component {
+    render() {
+     return (
+        <p>this is the header</p>
+     )
+
+    }
+
+}
+
+
+
+export default Header; 
